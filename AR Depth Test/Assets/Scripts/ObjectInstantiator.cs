86fadd7335201dfa88fd037c4f26236af554ae97
razi <razi@cravefx.com>
2021-01-07 20:08:51 +0800

    adding project

---
+++ AR Depth Test/Assets/Scripts/ObjectInstantiator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ObjectInstantiator : MonoBehaviour
+{
+	[SerializeField]
+	GameObject prefab;
+
+	ARRaycastManager arRaycastManager;
+
+	static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+	private void Awake()
+	{
+		arRaycastManager = GetComponent<ARRaycastManager>();
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+		{
+			Touch touch = Input.GetTouch(0);
+			Vector2 touchPos = touch.position;
+
+			if(touch.phase == TouchPhase.Began)
+			{
+				bool isOverUI = touchPos.IsPointOverUIObject();
+
+				if(!isOverUI && arRaycastManager.Raycast(touchPos, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+				{
+					var hitPose = hits[0].pose;
+					Instantiate(prefab, hitPose.position, hitPose.rotation);
+				}
+			}
+		}
+    }
+}
